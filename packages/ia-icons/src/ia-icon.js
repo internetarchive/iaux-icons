57a2b9e31555dd8b5cb72da4e13d90fe1cc74237
Shane Riley <shane@shaneriley.com>
2020-07-10 07:56:04 -0400

    Updates ia-icons to import icon packages

--- packages/ia-icons/src/ia-icon.js
+++ packages/ia-icons/src/ia-icon.js
@@ -1,18 +1,18 @@
 import { LitElement, html, css } from 'lit-element';
-import advance from './icons/advance';
-import audio from './icons/audio';
-import close from './icons/close';
-import donate from './icons/donate';
-import ellipses from './icons/ellipses';
-import iaLogo from './icons/ia-logo';
-import images from './icons/images';
-import search from './icons/search';
-import software from './icons/software';
-import texts from './icons/texts';
-import upload from './icons/upload';
-import user from './icons/user';
-import video from './icons/video';
-import web from './icons/web';
+import advance from '@internetarchive/icon-advance';
+import audio from '@internetarchive/icon-audio';
+import close from '@internetarchive/icon-close';
+import donate from '@internetarchive/icon-donate';
+import ellipses from '@internetarchive/icon-ellipses';
+import iaLogo from '@internetarchive/icon-ia-logo';
+import images from '@internetarchive/icon-images';
+import search from '@internetarchive/icon-search';
+import software from '@internetarchive/icon-software';
+import texts from '@internetarchive/icon-texts';
+import upload from '@internetarchive/icon-upload';
+import user from '@internetarchive/icon-user';
+import video from '@internetarchive/icon-video';
+import web from '@internetarchive/icon-web';
 
 const iconTemplates = {
   advance,
@@ -59,7 +59,5 @@ class IAIcon extends LitElement {
     return iconTemplates[this.icon] || html``;
   }
 }
-
-customElements.define('ia-icon', IAIcon);
 
 export default IAIcon;
