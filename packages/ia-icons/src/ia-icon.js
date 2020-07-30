fa404b825f320be94f1f3523cfef5daf8412a3ee
Jason Buckner <jason@jasonbuckner.com>
2020-07-30 11:52:14 -0700

    Update ia-icon and demo for new icons

--- packages/ia-icons/src/ia-icon.js
+++ packages/ia-icons/src/ia-icon.js
@@ -1,32 +1,50 @@
 import { LitElement, html, css } from 'lit-element';
 import advance from '@internetarchive/icon-advance';
+import applePay from '@internetarchive/icon-applepay';
 import audio from '@internetarchive/icon-audio';
+import calendar from '@internetarchive/icon-calendar';
 import close from '@internetarchive/icon-close';
+import creditCard from '@internetarchive/icon-credit-card';
 import donate from '@internetarchive/icon-donate';
 import ellipses from '@internetarchive/icon-ellipses';
+import email from '@internetarchive/icon-email';
+import googlePay from '@internetarchive/icon-googlepay';
 import iaLogo from '@internetarchive/icon-ia-logo';
 import images from '@internetarchive/icon-images';
+import localePin from '@internetarchive/icon-locale-pin';
+import lock from '@internetarchive/icon-lock';
+import paypal from '@internetarchive/icon-paypal';
 import search from '@internetarchive/icon-search';
 import software from '@internetarchive/icon-software';
 import texts from '@internetarchive/icon-texts';
 import upload from '@internetarchive/icon-upload';
 import user from '@internetarchive/icon-user';
+import venmo from '@internetarchive/icon-venmo';
 import video from '@internetarchive/icon-video';
 import web from '@internetarchive/icon-web';
 
 const iconTemplates = {
+  applePay,
   advance,
   audio,
+  calendar,
   close,
+  creditCard,
   donate,
   ellipses,
+  email,
+  googlePay,
   iaLogo,
   images,
+  localePin,
+  lock,
+  paypal,
   search,
   software,
   texts,
   upload,
   user,
+  venmo,
   video,
   web,
 };
