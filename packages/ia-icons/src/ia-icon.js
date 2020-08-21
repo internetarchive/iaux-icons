0aa747c0566c961311f62988b8549164e0feefa2
Shane Riley <shane@shaneriley.com>
2020-08-21 12:28:04 -0400

    Adds all item navigator icons

--- packages/ia-icons/src/ia-icon.js
+++ packages/ia-icons/src/ia-icon.js
@@ -3,24 +3,36 @@ import advance from '@internetarchive/icon-advance';
 import applePay from '@internetarchive/icon-applepay';
 import audio from '@internetarchive/icon-audio';
 import calendar from '@internetarchive/icon-calendar';
+import calendarBlank from '@internetarchive/icon-calendar-blank';
+import collapseSidebar from '@internetarchive/icon-collapse-sidebar';
 import close from '@internetarchive/icon-close';
 import creditCard from '@internetarchive/icon-credit-card';
 import donate from '@internetarchive/icon-donate';
+import download from '@internetarchive/icon-dl';
 import ellipses from '@internetarchive/icon-ellipses';
 import email from '@internetarchive/icon-email';
+import facebook from '@internetarchive/icon-facebook';
 import googlePay from '@internetarchive/icon-googlepay';
 import iaLogo from '@internetarchive/icon-ia-logo';
 import images from '@internetarchive/icon-images';
+import link from '@internetarchive/icon-link';
 import localePin from '@internetarchive/icon-locale-pin';
 import lock from '@internetarchive/icon-lock';
 import paypal from '@internetarchive/icon-paypal';
+import pinterest from '@internetarchive/icon-pinterest';
 import search from '@internetarchive/icon-search';
+import share from '@internetarchive/icon-share';
 import software from '@internetarchive/icon-software';
 import texts from '@internetarchive/icon-texts';
+import toc from '@internetarchive/icon-toc';
+import tumblr from '@internetarchive/icon-tumblr';
+import twitter from '@internetarchive/icon-twitter';
 import upload from '@internetarchive/icon-upload';
 import user from '@internetarchive/icon-user';
 import venmo from '@internetarchive/icon-venmo';
 import video from '@internetarchive/icon-video';
+import visualAdjustment from '@internetarchive/icon-visual-adjustment';
+import volumes from '@internetarchive/icon-volumes';
 import web from '@internetarchive/icon-web';
 
 const iconTemplates = {
@@ -28,24 +40,36 @@ const iconTemplates = {
   advance,
   audio,
   calendar,
+  calendarBlank,
+  collapseSidebar,
   close,
   creditCard,
   donate,
+  download,
   ellipses,
   email,
+  facebook,
   googlePay,
   iaLogo,
   images,
+  link,
   localePin,
   lock,
   paypal,
+  pinterest,
   search,
+  share,
   software,
   texts,
+  toc,
+  tumblr,
+  twitter,
   upload,
   user,
   venmo,
   video,
+  visualAdjustment,
+  volumes,
   web,
 };
 
