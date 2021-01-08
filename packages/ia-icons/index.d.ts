802fccad95d27232091957f3d14c10b7e7fa47e2
Jason Buckner <jason@jasonbuckner.com>
2021-01-08 14:16:15 -0800

    Add types to ia-icons so we can more easily use it in typescript

---
+++ packages/ia-icons/index.d.ts
@@ -0,0 +1,43 @@
+declare class IAIcon extends LitElement {
+  icon: string =
+    'applePay' |
+    'advance' |
+    'audio' |
+    'calendar' |
+    'calendarBlank' |
+    'close' |
+    'collapseSidebar' |
+    'creditCard' |
+    'donate' |
+    'download' |
+    'editPencil' |
+    'ellipses' |
+    'email' |
+    'facebook' |
+    'googlePay' |
+    'iaLogo' |
+    'images' |
+    'link' |
+    'localePin' |
+    'lock' |
+    'magnifyMinus' |
+    'magnifyPlus' |
+    'paypal' |
+    'pinterest' |
+    'search' |
+    'share' |
+    'software' |
+    'texts' |
+    'toc' |
+    'tumblr' |
+    'twitter' |
+    'upload' |
+    'user' |
+    'venmo' |
+    'video' |
+    'visualAdjustment' |
+    'volumes' |
+    'web';
+}
+
+export default IAIcon;
