e837e477942bdbb7d6840462268938e64598c341
Isa Herico Velasco <7840857+iisa@users.noreply.github.com>
2022-12-29 10:03:07 -0800

    ia-icons - point to svg

--- packages/ia-icons/src/ia-icon.js
+++ packages/ia-icons/src/ia-icon.js
@@ -1,46 +1,48 @@
-import { LitElement, html, css } from 'lit';
-import advance from '@internetarchive/icon-advance';
-import applePay from '@internetarchive/icon-applepay';
-import audio from '@internetarchive/icon-audio';
-import calendar from '@internetarchive/icon-calendar';
-import calendarBlank from '@internetarchive/icon-calendar-blank';
-import close from '@internetarchive/icon-close';
-import closeCircle from '@internetarchive/icon-close-circle';
-import collapseSidebar from '@internetarchive/icon-collapse-sidebar';
-import creditCard from '@internetarchive/icon-credit-card';
-import donate from '@internetarchive/icon-donate';
-import download from '@internetarchive/icon-dl';
-import editPencil from '@internetarchive/icon-edit-pencil';
-import ellipses from '@internetarchive/icon-ellipses';
-import email from '@internetarchive/icon-email';
-import facebook from '@internetarchive/icon-facebook';
-import googlePay from '@internetarchive/icon-googlepay';
-import iaLogo from '@internetarchive/icon-ia-logo';
-import images from '@internetarchive/icon-images';
-import link from '@internetarchive/icon-link';
-import localePin from '@internetarchive/icon-locale-pin';
-import lock from '@internetarchive/icon-lock';
-import magnifyMinus from '@internetarchive/icon-magnify-minus';
-import magnifyPlus from '@internetarchive/icon-magnify-plus';
-import paypal from '@internetarchive/icon-paypal';
-import pinterest from '@internetarchive/icon-pinterest';
-import search from '@internetarchive/icon-search';
-import share from '@internetarchive/icon-share';
-import software from '@internetarchive/icon-software';
-import sortAscending from '@internetarchive/icon-sort-ascending';
-import sortDescending from '@internetarchive/icon-sort-descending';
-import texts from '@internetarchive/icon-texts';
-import toc from '@internetarchive/icon-toc';
-import tumblr from '@internetarchive/icon-tumblr';
-import twitter from '@internetarchive/icon-twitter';
-import upload from '@internetarchive/icon-upload';
-import user from '@internetarchive/icon-user';
-import venmo from '@internetarchive/icon-venmo';
-import video from '@internetarchive/icon-video';
-import visualAdjustment from '@internetarchive/icon-visual-adjustment';
-import volumes from '@internetarchive/icon-volumes';
-import web from '@internetarchive/icon-web';
-import info from '@internetarchive/icon-info';
+import {
+  LitElement, html, css, nothing
+} from 'lit';
+import advance from '@internetarchive/icon-advance/index';
+import applePay from '@internetarchive/icon-applepay/index';
+import audio from '@internetarchive/icon-audio/index';
+import calendar from '@internetarchive/icon-calendar/index';
+import calendarBlank from '@internetarchive/icon-calendar-blank/index';
+import close from '@internetarchive/icon-close/index';
+import closeCircle from '@internetarchive/icon-close-circle/index';
+import collapseSidebar from '@internetarchive/icon-collapse-sidebar/index';
+import creditCard from '@internetarchive/icon-credit-card/index';
+import donate from '@internetarchive/icon-donate/index';
+import download from '@internetarchive/icon-dl/index';
+import editPencil from '@internetarchive/icon-edit-pencil/index';
+import ellipses from '@internetarchive/icon-ellipses/index';
+import email from '@internetarchive/icon-email/index';
+import facebook from '@internetarchive/icon-facebook/index';
+import googlePay from '@internetarchive/icon-googlepay/index';
+import iaLogo from '@internetarchive/icon-ia-logo/index';
+import images from '@internetarchive/icon-images/index';
+import link from '@internetarchive/icon-link/index';
+import localePin from '@internetarchive/icon-locale-pin/index';
+import lock from '@internetarchive/icon-lock/index';
+import magnifyMinus from '@internetarchive/icon-magnify-minus/index';
+import magnifyPlus from '@internetarchive/icon-magnify-plus/index';
+import paypal from '@internetarchive/icon-paypal/index';
+import pinterest from '@internetarchive/icon-pinterest/index';
+import search from '@internetarchive/icon-search/index';
+import share from '@internetarchive/icon-share/index';
+import software from '@internetarchive/icon-software/index';
+import sortAscending from '@internetarchive/icon-sort-ascending/index';
+import sortDescending from '@internetarchive/icon-sort-descending/index';
+import texts from '@internetarchive/icon-texts/index';
+import toc from '@internetarchive/icon-toc/index';
+import tumblr from '@internetarchive/icon-tumblr/index';
+import twitter from '@internetarchive/icon-twitter/index';
+import upload from '@internetarchive/icon-upload/index';
+import user from '@internetarchive/icon-user/index';
+import venmo from '@internetarchive/icon-venmo/index';
+import video from '@internetarchive/icon-video/index';
+import visualAdjustment from '@internetarchive/icon-visual-adjustment/index';
+import volumes from '@internetarchive/icon-volumes/index';
+import web from '@internetarchive/icon-web/index';
+import info from '@internetarchive/icon-info/index';
 
 const iconTemplates = {
   applePay,
@@ -117,7 +119,7 @@ class IAIcon extends LitElement {
   }
 
   render() {
-    return iconTemplates[this.icon] || html``;
+    return html`${iconTemplates[this.icon]}` || nothing;
   }
 }
 
